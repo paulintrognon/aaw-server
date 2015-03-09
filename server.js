e15d30f6def40464b4ad58986eca2d50f49c766a
Paulin Trognon <paulin.trognon@gmail.com>
2015-03-10 00:25:20 +0100

    now using app/logger to log server startup log

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 "use strict";
 
-var express = require('express');
+var express = require('express'),
+  logger = require('./app/logger');
 
 // Creating the app
 var app = express();
@@ -13,5 +14,5 @@ var server = app.listen(3000, function () {
   var host = server.address().address;
   var port = server.address().port;
 
-  console.log('Example app listening at http://%s:%s', host, port);
+  logger.info('Example app listening at http://%s:%s', host, port);
 });
